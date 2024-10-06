5db651a787a519186816a0eb3e1e92694a1f94e0
Andrew Nelson <nels4766@gmail.com>
2024-10-06 13:47:30 -0600

    Objective2Lab2

--- Labs/Objective2Lab2.java
+++ Labs/Objective2Lab2.java
@@ -1,11 +1,9 @@
 public class Objective2Lab2 {
   public static void main(String[] args) {
-    String name = "Andrew Nelson";
-    int age = 46;
-    String state = "Minnesota";
+    String name = "Name: Andrew Nelson";
+    int age = "Age: 46";
+    String state = "State: Minnesota";
 
-    System.out.println(name);
-    System.out.println(age);
-    System.out.println(state);
+    System.out.println(name age state);
   }
 }
