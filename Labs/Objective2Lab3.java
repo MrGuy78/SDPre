45a319cbd3408ce868e70e1970b7f9f5842b0429
Andrew Nelson <nels4766@gmail.com>
2024-10-06 14:20:02 -0600

    Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    int year = 1978;
+    string month = "September";
+    boolean = true;
+
+    System.out.println(int);
+    System.out.println(string);
+    System.out.println(boolean);
+
+  }
+}
