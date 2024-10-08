b14e285896192b7a59c074267e559b4f0703581d
Andrew Nelson <nels4766@gmail.com>
2024-10-07 21:28:59 -0600

    Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,15 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    double num1 = 4;
+    System.out.println(num1);
+    double num2 = 10;
+    System.out.println(num2);
+    double num3 = 13;
+    System.out.println(num3);
+    double average = 3;
+    System.out.println(9);
+
+  }
+}
