d7a5546c01b7deec6ffb68fea84cd8fad6e36eda
Andrew Nelson <nels4766@gmail.com>
2024-10-07 22:36:16 -0600

    Objective3Lab3

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -4,6 +4,6 @@ public class Objective3Lab3 {
     int first = 1 + (int)(Math.random() * 6);
     int second = 1 + (int)(Math.random() * 6);
     int third = 1 + (int)(Math.random() * 6);
-    System.out.println("first + second + third = (first + second + third)");
+    System.out.println(first) + (second) + (third) = (first + second + third);
   }
 }
