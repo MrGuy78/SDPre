78ae6921da76f7b5e298ad9705a337a2738a1cda
Andrew Nelson <nels4766@gmail.com>
2024-10-07 22:02:13 -0600

    Objective3Lab2

--- Labs/Objective3Lab2.java
+++ Labs/Objective3Lab2.java
@@ -3,7 +3,7 @@ public class Objective3Lab2 {
 
   	double side1 = 12;
   	double side2 = 15;
-  	double hypotenuse = Match.sqrt(Math.sqrt(side1 * side1) + Math.sqrt(side2 * side2));
+  	double hypotenuse = Math.sqrt(Math.sqrt(side1 * side1) + Math.sqrt(side2 * side2));
   	System.out.println(hypotenuse);
 
   }
