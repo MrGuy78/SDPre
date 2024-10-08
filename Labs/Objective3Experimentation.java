02991594d02789e94536c6f501b570c084ca3177
Andrew Nelson <nels4766@gmail.com>
2024-10-07 21:11:28 -0600

    Objective3Experimentation

--- Labs/Objective3Experimentation.java
+++ Labs/Objective3Experimentation.java
@@ -1,6 +1,10 @@
-int num6 = 10;
+public class HelloWorld {
+    public static void main(String[] args) {
+      int num6 = 10;
 num6++;
 System.out.println(num6);
 
 int num7 = 10;
 System.out.println(num7);
+    }
+}
