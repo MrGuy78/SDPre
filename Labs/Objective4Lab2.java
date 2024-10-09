868211f7752e35d844fc16b552039062eb84d6c3
Andrew Nelson <nels4766@gmail.com>
2024-10-08 22:03:20 -0600

    Objective4Lab2

---
+++ Labs/Objective4Lab2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Objective4Lab2 {
+  public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.println("Choose a whole number.");
+    int num1 = keyboard.nextLine();
+    System.out.println("Choose another whole number.");
+    int num2 = keyboard.nextLine();
+    System.out.println("Choose one last whole number.");
+    int num3 = keyboard.nextLine();
+    int wholeAnswer = (num1 + num2 + num3)
+    System.out.println("Choose a decimal number.");
+    double dub1 = keyboard.nextDouble();
+    System.out.println("Choose another decimal number.");
+    double dub2 = keyboard.nextDouble();
+    System.out.println("Choose a final decimal number.");
+    double dub3 = keyboard.nextDouble();
+    double decimalAnswer = (dub1 + dub2 + dub3);
+
+    System.out.println(num1 + "+" + num2 + "+" + num3 "=" wholeAnswer);
+    System.out.println(dub1 + "+" + dub2 + "+" + dub3 "=" decimalAnswer);
+
+    keyboard.close();
+    
+  }
+}
