54bc947049936926d5cadf24b9f4d89112de4eae
Andrew Nelson <nels4766@gmail.com>
2024-10-09 14:53:44 -0600

    SwitchExample.java

---
+++ Labs/SwitchExample.java
@@ -0,0 +1,23 @@
+public class SwitchExample {
+	public static void main(String[] args) {
+		char grade;
+		grade = 'B';
+
+		switch (grade) {
+		case "A":
+		  System.out.println("Fantastic job!");
+		  break;
+		case 'B';
+		  System.out.println("Good job!");
+		  break;
+		case 'C';
+		  System.out.println("You're doing okay.");
+		case 'D';
+		  System.out.println("Please schedule a conference.");
+		case 'F';
+		  System.out.println("Start finishing homework and showing up to class.");
+		default:
+		  System.out.println("Invalid grade.");
+		}
+	}
+}
