4afa9289fc680c888a8c3f0c5aa9295d6b13d940
Andrew Nelson <nels4766@gmail.com>
2024-10-09 11:16:30 -0600

    Objective4Lab3

--- Labs/Objective4Lab3.java
+++ Labs/Objective4Lab3.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 public class Objective4Lab3 {
   public static void main(String[] args) {
-    int age = input.nextInt();
-    int currentYear = 2024;
-    int birthYear = (currentYear - age);
   
    Scanner input = new Scanner(System.in);
 
     System.out.println("How old are you?");
     input.nextInt();
+    int age = input.nextInt();
+    int currentYear = 2024;
+    int birthYear = (currentYear - age);
 
     System.out.println("You were born in " + birthYear + ".");
     input.close();
