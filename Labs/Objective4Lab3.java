d1216330a3032f67c6ae371441ccede2d503d7b6
Andrew Nelson <nels4766@gmail.com>
2024-10-09 11:07:15 -0600

    Objective4Lab3

--- Labs/Objective4Lab3.java
+++ Labs/Objective4Lab3.java
@@ -6,13 +6,13 @@ public class Objective4Lab3 {
   
    Scanner input = new Scanner(System.in);
 
-    System.out.println("How old are you?");
-    input.nextInt();
     int age = input.nextInt();
     int currentYear = 2024;
     int birthYear = (currentYear - age);
+    System.out.println("How old are you?");
+    input.nextInt();
 
-    System.out.println("You were born in " + birthYear);
+    System.out.println("You were born in " + birthYear + ".");
     input.close();
   }
 }
