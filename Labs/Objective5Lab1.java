f74a7445656cbcc9e4fa8f54ec3db37bc25d6e37
Andrew Nelson <nels4766@gmail.com>
2024-10-09 13:20:34 -0600

    Objective5Lab1

--- Labs/Objective5Lab1.java
+++ Labs/Objective5Lab1.java
@@ -1,7 +1,7 @@
 public class Objective5Lab1 {
   public static void main(String[] args) {
-    int num1 = 5;
-    int num2 = 15;
+    int num1 = 6;
+    int num2 = 24;
 
     if(num1 > num2) {
     System.out.println(num1 + " is greater than " + num2 + ".");
