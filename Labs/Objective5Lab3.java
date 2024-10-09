d3320e84c8bacc3719a10f73a3d0a17df9aaf20d
Andrew Nelson <nels4766@gmail.com>
2024-10-09 13:49:11 -0600

    Objective5Lab3

--- Labs/Objective5Lab3.java
+++ Labs/Objective5Lab3.java
@@ -1,7 +1,6 @@
 public class Objective5Lab3 {
     public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
-      int userNum;
 
       System.out.println("Please enter a number: ");
       int userNum = input.nextInt();
