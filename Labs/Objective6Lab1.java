e9cc37efcea5c9f383680dc426b361b2dd0f2f0a
Andrew Nelson <nels4766@gmail.com>
2024-10-09 15:19:43 -0600

    Objective6Lab1

--- Labs/Objective6Lab1.java
+++ Labs/Objective6Lab1.java
@@ -4,40 +4,40 @@ public class Objective6Lab1 {
 
     switch (month) {
       case 1:
-        System.out.println("1 -> January");
+        System.out.println("January");
         break;
       case 2:
-        System.out.println("2 -> February");
+        System.out.println("February");
         break;
       case 3:
-        System.out.println("3 -> March");
+        System.out.println("March");
         break;
       case 4:
-        System.out.println("4 -> April");
+        System.out.println("April");
         break;
       case 5:
-        System.out.println("5 -> May");
+        System.out.println("May");
         break;
       case 6:
-        System.out.println("6 -> June");
+        System.out.println("June");
         break;
       case 7:
-        System.out.println("7 -> July");
+        System.out.println("July");
         break;
       case 8:
-        System.out.println("8 -> August");
+        System.out.println("August");
         break;
       case 9:
-        System.out.println("9 -> September");
+        System.out.println("September");
         break;
       case 10:
-        System.out.println("10 -> October");
+        System.out.println("October");
         break;
       case 11:
-        System.out.println("11 -> November");
+        System.out.println("November");
         break;
       case 12:
-        System.out.println("12 -> December");
+        System.out.println("December");
         break;
       default:
         System.out.println("That is not a valid month");
