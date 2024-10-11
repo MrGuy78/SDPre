54f27788d050ca4c477cc85c8f83fd255cd248bc
Andrew Nelson <nels4766@gmail.com>
2024-10-11 12:59:59 -0600

    Objective8Lab1

--- Labs/Objective8Lab1.java
+++ Labs/Objective8Lab1.java
@@ -1,11 +1,13 @@
 public class Objective8Lab1 {
   public static void main(String[] args) {
+    int i = 0;
+    int j =10;
  
-    for (int i = 0; i<=10; i+=1) {
+    for (int i=0; i<=10; i+=1) {
       System.out.println(i);
     }
 
-    for (int j = 10; j >=10; i-=1) {
+    for (int j=10; j >=10; i-=1) {
       System.out.println(j);
     }
 
