34ad8ac509a009d4b274cf2dd7c0d8e25cfc5e8c
Andrew Nelson <nels4766@gmail.com>
2024-10-10 20:04:55 -0600

    Objective5Lab4

--- Labs/Objective5Lab4.java
+++ Labs/Objective5Lab4.java
@@ -1,14 +1,14 @@
 public class Objective5Lab3 {
     public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
+      java.util.Scanner scanner = new java.util.Scanner(System.in);
 
       System.out.println("Please enter a number: ");
-      int userNum = input.nextInt();
+      int userNum = scanner.nextInt();
 
       if(userNum % 2 == 0) {
         System.out.println("The number is even");
       }
-      else(userNum % 2 != 0) {
+      else {
         System.out.println("The number is odd");
       }
 
