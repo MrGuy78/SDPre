eeb1e859b5b62b9e5576ef9f110b3eb3883338d1
Andrew Nelson <nels4766@gmail.com>
2024-10-10 20:55:37 -0600

    Objective7Lab3

--- Labs/Objective7Lab3.java
+++ Labs/Objective7Lab3.java
@@ -3,7 +3,13 @@ public class Objective7Lab3 {
     int counter = 1;
 
     while(counter <= 20) {
-      System.out.println(counter);
+
+      if(counter % 2 == 0) {
+        System.out.println(counter + " is even.");
+    }
+      else {
+        System.out.println(counter + " is odd.")
+      }
 
       counter++;
     }
