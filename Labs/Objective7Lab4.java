a5ad7659e238b99985ddf0e443942cb750701958
Andrew Nelson <nels4766@gmail.com>
2024-10-10 21:29:31 -0600

    Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab3 {
+  public static void main(String[] args) {
+    int sum
+    int count = 0
+
+    do {
+      count++;
+
+    } while(count <= 20);
+
+    sum = (count);
+  }
+}
