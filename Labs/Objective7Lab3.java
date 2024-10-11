84ae0ad86272ee494fa82a7887744e232fada5a5
Andrew Nelson <nels4766@gmail.com>
2024-10-10 20:49:33 -0600

    Objective7Lab3

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,12 @@
+public class Objective7Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+
+    while(counter <= 20) {
+      System.out.println(counter);
+
+      counter++;
+    }
+    
+  }
+}
