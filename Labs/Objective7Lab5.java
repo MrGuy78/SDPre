712ea0485a4d56770edc1c94d01db32ddc21e00c
Andrew Nelson <nels4766@gmail.com>
2024-10-11 12:26:28 -0600

    Objective7Lab5

--- Labs/Objective7Lab5.java
+++ Labs/Objective7Lab5.java
@@ -4,7 +4,7 @@ public class Objective7Lab5 {
     int selection;
 
     while (true) {
-      if(selection = 1);
+      if (selection = 1);
       System.out.println("Hello World");
       System.out.println("_____Menu_____");
       System.out.println("1: Say Hello");
@@ -12,7 +12,7 @@ public class Objective7Lab5 {
       System.out.println("3: Exit");
       System.out.println();
 
-      else(selection = 2);
+      else if (selection = 2);
       System.out.println("Apples, Bananas, Coconuts");
       System.out.println("_____Menu_____");
       System.out.println("1: Say Hello");
@@ -20,8 +20,9 @@ public class Objective7Lab5 {
       System.out.println("3: Exit");
       System.out.println();
 
-      else(selection = 3);
+      else if (selection = 3);
       System.out.println("Goodbye");
+
       selection = scanner.nextInt();
 
     }
