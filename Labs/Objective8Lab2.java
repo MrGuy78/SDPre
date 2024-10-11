9a03dd98e01a75aa6a37309de33618db54b585f4
Andrew Nelson <nels4766@gmail.com>
2024-10-11 13:38:36 -0600

    Objective8Lab2

--- Labs/Objective8Lab2.java
+++ Labs/Objective8Lab2.java
@@ -2,7 +2,8 @@ public class Objective8Lab2 {
   public static void main(String[] args) {
     int sum = 0;
 
-    for (count=0; count<=20; (count++)+sum) {
+    for (count=0; count<=20; count+sum) {
+      count++;
       System.out.println(sum);
     }
 
