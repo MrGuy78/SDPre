2ed7111c1a9af156b5e752bb567347134617bc62
Andrew Nelson <nels4766@gmail.com>
2024-10-11 21:28:12 -0600

    Objective8Lab3

--- Labs/Objective8Lab3.java
+++ Labs/Objective8Lab3.java
@@ -3,7 +3,7 @@ public class Objective8Lab3 {
     int counter = 1;
 
     for (counter = 1; counter <= 20; counter++) {
-      if(counter % 2 = 0) {
+      if(counter % 2 == 0) {
         System.out.println(counter + " is even.");
       }
       else {
