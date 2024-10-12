b400f1d33942fcf801aca47a4a3c19aa294804ed
Andrew Nelson <nels4766@gmail.com>
2024-10-11 20:01:49 -0600

    Objective7Lab5

--- Labs/Objective7Lab5.java
+++ Labs/Objective7Lab5.java
@@ -4,30 +4,23 @@ public class Objective7Lab5 {
     int selection;
 
     while (true) {
-      if (selection = 1) {
-      System.out.println("Hello World");
       System.out.println("_____Menu_____");
       System.out.println("1: Say Hello");
       System.out.println("2: List My favorite foods");
       System.out.println("3: Exit");
       System.out.println();
-    }
-
-      if (selection = 2) {
-      System.out.println("Apples, Bananas, Coconuts");
-      System.out.println("_____Menu_____");
-      System.out.println("1: Say Hello");
-      System.out.println("2: List My favorite foods");
-      System.out.println("3: Exit");
-      System.out.println();
-    }
-
-      if (selection = 3) {
-      System.out.println("Goodbye");
-    }
 
       selection = scanner.nextInt();
 
+      if(selection = 1) {
+        System.out.println("Hello World");
+    }
+      if(selection = 2){
+        System.out.println("Apples, Bananas, Coconuts");
+    }
+      if(selection = 3) {
+        System.out.println("Goodbye");
+      }
     }
 
     scanner.close();
